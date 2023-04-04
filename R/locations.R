aceb62984b108551e50ec901cdc42c4d1a8bc43c
Lars Schöbitz <lars@lse.de>
2023-04-04 15:10:39 +0200

    Remove line from documentation

--- R/locations.R
+++ R/locations.R
@@ -1,7 +1,5 @@
 #' Plastic waste litterboom sampling locations
 #'
-#' Weight in kg of identified PET and HDPE/PP items.
-#'
 #' @format A tibble with 6 rows and 3 variables:
 #' \describe{
 #'   \item{location}{Descriptive name of the sample location.}
